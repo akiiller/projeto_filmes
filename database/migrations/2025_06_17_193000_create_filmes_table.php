f805952a680a44f0a8f15223ef7bcf178acd738d
akiiller <admkiiller@gmail.com>
2025-06-17 16:35:11 -0300

    commit inicial

---
+++ database/migrations/2025_06_17_193000_create_filmes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filmes', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('diretor');
+            $table->integer('ano_lancamento');
+            $table->string('genero');
+            $table->string('imagem')->nullable(); // Caminho da imagem
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filmes');
+    }
+};
